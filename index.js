5a379d281cc5e681411a2d0bb46ea1a202880f1c
jasonemarkson <jasonmarkson76@gmail.com>
2021-03-20 14:52:36 -0400

    commit tree 12e759427bba821f8aac9bcdd14f571a267199b1

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 // Add your functions here
-function mapToNegativize(array) {
-    let a = array.map(element => element * -1)
+function map(array, callback) {
+
+    let a = []
+    for(let element of array) {
+        a.push(callback(element))
+    }
     return a
+}
+
+function reduce(array, callback, startingPoint) {
+    let total
+    let i
+
+    if (startingPoint) {
+        i = 0
+        total = startingPoint
+    } else {
+        i = 1
+        total = array[0]
+    }
+
+    // for(let element of array) {
+    //     total = callback(element, total)
+    // }
+    // return total
+
+    for (i; i < array.length; i++) {
+        total = callback(array[i], total)
+    }
+    return total
 }
