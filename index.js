bd45ae1e750fa52f74ff3db25c779ddf502d2061
jasonemarkson <jasonmarkson76@gmail.com>
2021-06-10 21:51:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 // Add your functions here
-function mapToNegativize(array) {
-    let a = array.map(element => element * -1)
+function map(array, callback) {
+
+    let a = []
+    for(let element of array) {
+        a.push(callback(element))
+    }
     return a
+}
+
+function reduce(array, callback, startingPoint) {
+    let total
+    let i
+
+    if (startingPoint) {
+        i = 0
+        total = startingPoint
+    } else {
+        i = 1
+        total = array[0]
+    }
+
+    for (i; i < array.length; i++) {
+        total = callback(array[i], total)
+    }
+    return total
 }
